9b2c59e374584b23df75dfa09b86cdec79653c0c
finnp <derfinn@gmail.com>
2016-07-22 16:59:23 +0200

    feat: allow modifying of files in subfolders

--- src/content-from-filename.js
+++ src/content-from-filename.js
@@ -12,19 +12,13 @@ module.exports = async function contentFromFilename (github, config) {
   const addRepo = defaultDefault(pick(config, ['user', 'repo']))
 
   try {
-    const head = await promisify(github.gitdata.getReference)(addRepo({ref: `heads/${branch}`}))
+    const blob = await promisify(github.repos.getContent)(addRepo({path: filename, ref: branch}))
 
-    const { tree } = await promisify(github.gitdata.getTree)(addRepo({sha: head.object.sha}))
-
-    const { sha } = tree.find((object) => object.path === filename)
-
-    if (!sha) return Promise.reject(new Error(`Couldn't find ${filename}.`))
-
-    const blob = await promisify(github.gitdata.getBlob)(addRepo({sha}))
+    if (blob.type !== 'file') return Promise.reject(new Error('Type is not a file'))
 
     return Promise.resolve({
       content: (new Buffer(blob.content, 'base64')).toString(),
-      commit: head.object.sha
+      commit: blob.sha
     })
   } catch (err) {
     return Promise.reject(err)
--- src/update-file-with-content.js
+++ src/update-file-with-content.js
@@ -4,31 +4,23 @@ const promisify = require('es6-promisify')
 const defaultDefault = require('./default-default')
 
 module.exports = async function (github, config) {
-  const { content, filename, sha, author, committer } = config
+  const { branch, content, filename, sha, author, committer } = config
   const message = config.message || `chore: updated ${filename}`
 
   const addRepo = defaultDefault(pick(config, ['user', 'repo']))
 
   try {
-    const tree = await promisify(github.gitdata.createTree)(addRepo({
-      base_tree: sha,
-      tree: [{
-        path: filename,
-        mode: '100644',
-        type: 'blob',
-        content
-      }]
-    }))
-
-    const commit = await promisify(github.gitdata.createCommit)(addRepo({
+    const response = await promisify(github.repos.updateFile)(addRepo({
+      path: filename,
       message,
-      tree: tree.sha,
-      parents: [sha],
-      author,
-      committer
+      content: (new Buffer(content, 'utf8')).toString('base64'),
+      sha,
+      branch,
+      committer,
+      author
     }))
 
-    return Promise.resolve(commit)
+    return Promise.resolve(response.commit)
   } catch (err) {
     return Promise.reject(err)
   }
