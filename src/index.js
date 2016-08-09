329df2adfedbc390ff2f8e9a6524b8e6cfeae91c
Christoph Witzko <github@christophwitzko.com>
2016-08-09 16:56:59 +0200

    feat: use GitHub content api
    BREAKING CHANGE: remove pull request option

--- src/index.js
+++ src/index.js
@@ -1,75 +1,44 @@
-require('babel-core/polyfill')
-
-const { defaults } = require('lodash')
+const {defaults} = require('lodash')
 const GitHubApi = require('github')
-const promisify = require('es6-promisify')
+const {promisify} = require('bluebird')
 
 const contentFromFilename = require('./content-from-filename')
 const updateFileWithContent = require('./update-file-with-content')
 
-module.exports = async function (config, callback) {
+module.exports = async function (config) {
   const {
     branch = 'master',
+    newBranch,
     token,
     transform,
     force
   } = config
 
-  try {
-    let github = config.github
+  let github = config.github
 
-    if (!github) {
-      github = new GitHubApi({
-        version: '3.0.0'
-      })
-
-      github.authenticate({type: 'oauth', token})
-    }
-
-    const content = await contentFromFilename(github, config)
-    const newContent = transform(content.content)
-
-    var transformedConfig = {}
-    if (typeof newContent === 'string') transformedConfig.content = newContent
-    else transformedConfig = newContent
-
-    config = defaults(transformedConfig, {sha: content.commit}, config)
-
-    const commit = await updateFileWithContent(github, config)
-
-    const {
-      push,
-      pr,
-      newBranch
-    } = config
-
-    if (!(pr || push || newBranch)) return callback(null, commit)
-
-    if (push) {
-      return github.gitdata.updateReference(
-        defaults({
-          ref: `refs/heads/${branch}`,
-          sha: commit.sha
-        }, config),
-        callback
-      )
-    }
-
-    if (newBranch) {
-      await promisify(github.gitdata[force ? 'updateReference' : 'createReference'])(defaults({
-        sha: commit.sha,
-        ref: (force ? '' : 'refs/') + 'heads/' + newBranch,
-        force
-      }, config))
-    }
-
-    if (!pr) return callback(null, {commit})
-
-    github.pullRequests.create(defaults(pr, config, {
-      base: branch,
-      head: config.newBranch || commit.sha
-    }), callback)
-  } catch (err) {
-    callback(err)
+  if (!github) {
+    github = new GitHubApi()
+    github.authenticate({type: 'oauth', token})
   }
+
+  const reference = await promisify(github.gitdata.getReference)(defaults({
+    ref: `heads/${branch}`
+  }, config))
+
+  await promisify(github.gitdata[force ? 'updateReference' : 'createReference'])(defaults({
+    sha: reference.object.sha,
+    ref: (force ? '' : 'refs/') + `heads/${newBranch}`,
+    force
+  }, config))
+
+  const content = await contentFromFilename(github, config)
+  const newContent = transform(content.content)
+
+  var transformedConfig = {}
+  if (typeof newContent === 'string') transformedConfig.content = newContent
+  else transformedConfig = newContent
+
+  const commit = await updateFileWithContent(github, defaults(transformedConfig, {sha: content.commit}, config))
+
+  return commit
 }
